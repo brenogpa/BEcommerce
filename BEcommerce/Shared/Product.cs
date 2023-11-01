d9eb15465aa8968b03db6a22eefa2f07a0dc7b27
Breno <brenogpa@gmail.com>
2023-11-01 15:27:05 -0300

    Create Product.cs

---
+++ BEcommerce/Shared/Product.cs
@@ -0,0 +1,10 @@
+namespace BEcommerce.Shared;
+
+public class Product
+{
+    public int id { get; set; }
+    public string title { get; set; } = string.Empty;
+    public string description { get; set; } = string.Empty;
+    public string imageUrl { get; set; } = string.Empty;
+    public decimal price { get; set; }
+}
