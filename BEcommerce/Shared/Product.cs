b646147b71ddcc9bdc09cd092394dc572a220f39
Breno <brenogpa@gmail.com>
2023-11-01 16:57:38 -0300

    Update Product.cs

--- BEcommerce/Shared/Product.cs
+++ BEcommerce/Shared/Product.cs
@@ -2,9 +2,9 @@ namespace BEcommerce.Shared;
 
 public class Product
 {
-    public int id { get; set; }
-    public string title { get; set; } = string.Empty;
-    public string description { get; set; } = string.Empty;
-    public string imageUrl { get; set; } = string.Empty;
-    public decimal price { get; set; }
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string ImageUrl { get; set; } = string.Empty;
+    public decimal Price { get; set; }
 }
