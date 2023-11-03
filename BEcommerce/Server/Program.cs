2dc1d6aa760870f19a7d2e49c2f89ef4ffbcc583
Breno <brenogpa@gmail.com>
2023-11-03 11:27:34 -0300

    Update Program.cs
    DefaultConnection

--- BEcommerce/Server/Program.cs
+++ BEcommerce/Server/Program.cs
@@ -1,10 +1,16 @@
 global using BEcommerce.Shared;
 global using Microsoft.EntityFrameworkCore;
+using BEcommerce.Server.Data;
 using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
