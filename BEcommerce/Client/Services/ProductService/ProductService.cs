5b62707bfc14029ea6ee35bc262b1ffd874ca10d
Breno <brenogpa@gmail.com>
2023-11-07 12:00:01 -0300

    Changed GetProducts method
    - Get products with categoryUrl
    - Event Action ProductsChanged
    - Injected GetProducts on Index.razor

--- BEcommerce/Client/Services/ProductService/ProductService.cs
+++ BEcommerce/Client/Services/ProductService/ProductService.cs
@@ -10,17 +10,23 @@ public class ProductService : IProductService
     }
 
     public List<Product> Products { get; set; } = new List<Product>();
+    public event Action ProductsChanged;
     
-    public async Task GetProducts()
-    {
-        var result = await _httpClient.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product");
-        if (result != null && result.Data != null)
-            Products = result.Data;
-    }
-
     public async Task<ServiceResponse<Product>> GetProduct(int productId)
     {
         var result = await _httpClient.GetFromJsonAsync<ServiceResponse<Product>>($"api/product/{productId}");
         return result;
+    }
+    
+    public async Task GetProducts(string? categoryUrl = null)
+    {
+        var result = categoryUrl == null
+            ? await _httpClient.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product")
+            : await _httpClient.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/category/{categoryUrl}");
+            
+        if (result != null && result.Data != null)
+            Products = result.Data;
+        
+        ProductsChanged.Invoke();
     }
 }
