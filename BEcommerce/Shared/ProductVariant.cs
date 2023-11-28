264d3e6eddd94f0e18ec10e105da6fc927ec9a28
Breno <brenogpa@gmail.com>
2023-11-28 11:38:20 -0300

    Including Product Variants

--- BEcommerce/Shared/ProductVariant.cs
+++ BEcommerce/Shared/ProductVariant.cs
@@ -7,12 +7,16 @@ public class ProductVariant
 {
     [JsonIgnore]
     public Product Product { get; set; }
+    
     public int ProductId { get; set; }
+    
     public ProductType ProductType { get; set; }
+    
     public int ProductTypeId { get; set; }
 
-    [Column(TypeName = "decimal(18,2")]
+    [Column(TypeName = "decimal(18,2)")]
     public decimal Price { get; set; }
-    [Column(TypeName = "decimal(18,2")]
+    
+    [Column(TypeName = "decimal(18,2)")]
     public decimal OriginalPrice { get; set; }
 }
