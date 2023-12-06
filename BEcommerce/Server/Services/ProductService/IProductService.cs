fd9697215561ce484672dac2a9ed5e10acbd4a42
Breno <brenogpa@gmail.com>
2023-12-06 18:37:16 -0300

    SearchProducts

--- BEcommerce/Server/Services/ProductService/IProductService.cs
+++ BEcommerce/Server/Services/ProductService/IProductService.cs
@@ -5,4 +5,5 @@ public interface IProductService
     Task<ServiceResponse<List<Product>>> GetProductsAsync();
     Task<ServiceResponse<Product>> GetProductAsync(int productId);
     Task<ServiceResponse<List<Product>>> GetProductsByCategory(string categoryUrl);
+    Task<ServiceResponse<List<Product>>> SearchProducts(string searchText);
 }
--- BEcommerce/Server/Services/ProductService/ProductService.cs
+++ BEcommerce/Server/Services/ProductService/ProductService.cs
@@ -51,4 +51,19 @@ public class ProductService : IProductService
 
         return response;
     }
+
+    public async Task<ServiceResponse<List<Product>>> SearchProducts(string searchText)
+    {
+        var response = new ServiceResponse<List<Product>>
+        {
+            Data = await _context.Products
+                .Where(x => x.Title.ToLower().Contains(searchText.ToLower())
+                            ||
+                            x.Description.ToLower().Contains(searchText.ToLower()))
+                .Include(x => x.Variants)
+                .ToListAsync()
+        };
+
+        return response;
+    }
 }
